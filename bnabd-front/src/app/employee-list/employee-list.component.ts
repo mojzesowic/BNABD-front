4d749a9156fed51e742db97cbdca732d5dd4777b
Jakub Wisniewski <jakubwisn@gmail.com>
2021-01-12 23:30:55 +0100

    connect to backend

--- bnabd-front/src/app/employee-list/employee-list.component.ts
+++ bnabd-front/src/app/employee-list/employee-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Employee } from '../employee';
+import { EmployeeService } from '../employee.service';
 
 @Component({
   selector: 'app-employee-list',
@@ -10,21 +11,16 @@ export class EmployeeListComponent implements OnInit {
 
   employees!: Employee[];
   
-  constructor() { }
+  constructor(private employeeService: EmployeeService) { }
 
   ngOnInit(): void {
-    this.employees = [{
-      "id": 1,
-      "firstName": "dd",
-      "lastName": "ddee",
-      "emailId": "dd@ddee.com"
-    },
-    {
-      "id": 2,
-      "firstName": "ff",
-      "lastName": "ffee",
-      "emailId": "ff@ffee.com"
-    }];
+    this.getEmployees();
+  }
+
+  private getEmployees() {
+    this.employeeService.getEmpoyeesList().subscribe(data => {
+      this.employees = data;
+    });
   }
 
 }
